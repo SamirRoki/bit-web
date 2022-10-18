d2bd1e553e9ed1073f195fd92d1f7424c9661459
Lejla Čarovac-Mehmedović <samir_mehmedovic@live.com>
2022-10-18 20:14:32 +0200

    first commit

---
+++ pp/conditionals/conditionals.js
@@ -0,0 +1,28 @@
+//Write a program that compares
+//two numbers and display the
+//larger. Result should be
+//displayed in the console.
+
+
+var a = 2;
+var b = 3;
+var result = "";
+if(a > b ) { result = a + "is greater than b";
+
+}
+else if(a < b) {result = b + "is greater than a";}
+console.log (result);
+ 
+// Write a program to check if the
+// number is divisible by 2. If it
+// is, print even, if not, print
+// odd.
+
+
+var a = 3;
+var result = " ";
+
+if (a % 2 = 0) {result = even};
+else  {result = odd};
+
+console.log (result);
---
+++ pp/variables Tipes Operation/operators.js
@@ -0,0 +1,68 @@
+/* var yourName = "samir";
+var age = 35;
+var fullName = "samir mehmedovic";
+var _informations = "all informations";
+var mix12three = "20";
+console.log(yourName);
+console.log(_informations);
+
+
+//operators
+var a = 3+4;
+console.log(typeof a);
+var b = 1.2*1000;
+console.log(b);
+var ana = 5*60;
+var tom = 30*60;
+var result = tom-ana;
+ console.log(result);
+var d = typeof null;
+console.log(d);
+/* od 1 do 12 vezbe */
+
+
+
+
+
+
+
+
+
+
+
+
+//logical operators
+
+
+
+
+
+
+
+var p = false;
+var q = false;
+console.log(p && q);
+console.log(p || q);
+
+
+
+
+var age = 30;
+console.log(age >120);
+console.log(age<0); */
+
+
+var a = 60;
+var b = 120;
+
+
+console.log(a >=60 && b <= 120);
+
+
+
+
+
+
+
+
+
